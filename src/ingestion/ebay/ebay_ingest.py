310ffe3af77aa89dd536f561e4e171bc3da9462a
Gene Lee <dacgene.lee@gmail.com>
2026-04-29 13:55:42 -0400

    Updated top n cards to 1000

--- src/ingestion/ebay/ebay_ingest.py
+++ src/ingestion/ebay/ebay_ingest.py
@@ -335,7 +335,7 @@ def parse_args():
     parser.add_argument(
         "--top-n-cards",
         type=int,
-        default=750,
+        default=1000,
         help="Number of top cards from weekly_top_tcg_cards to query on eBay. Default: 300.",
     )
     parser.add_argument(
